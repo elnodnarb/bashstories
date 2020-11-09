5f8e93f4e87c62e57842f862f5c3b26d87e47d80
elnodnarb <elnodnarb@git.com>
2020-11-09 16:10:51 -0500

    Updating history for 2020/11/09.txt

--- bashstory_saver.sh
+++ bashstory_saver.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-set -o history
+#set -o history
 cd ~/bashstories
 today_date=$(date +%Y/%m)
 mkdir -p $today_date
 today_name=$(date +%d)
 cat ~/.bash_history >> ~/bashstories/${today_date}/${today_name}.txt
-git add ~/bashstories/${today_date}/${today_name}.txt
+git add ~/bashstories/* -A 
+#git add ~/bashstories/${today_date}/${today_name}.txt
 git commit -m "Updating history for ${today_date}/${today_name}.txt"
 git push
 exit 0
